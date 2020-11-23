5fcb0157d47d7a9d173cadb826f8d51987eb9eb9
Adrián Utrera Ruiz <adrianutreraruiz@gmail.com>
2020-11-23 02:28:13 +0100

    Creamos los atributos de fila y columna,creamos los metodos de get,set.

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
@@ -4,6 +4,32 @@ package org.iesalandalus.programacion.alfilajedrez;
 public class Posicion {
 
 	private int fila;
-	private char colunmna;
+	private char columna;
+	
+	
+	public Posicion(int fila, char columna) {
+		setFila(fila);
+		setColumna(columna);		
+	}
+	
+	public int getFila() {
+		return fila;
+	}
+	private void setFila(int fila) {
+		if (fila <1 || fila >8) {
+			throw new IllegalArgumentException("Error: Fila no válida.");
+		}		
+		this.fila = fila;
+	}
+	public char getColunmna() {
+		return columna;
+	}
+	private void setColumna(char columna) {
+		if (columna < 'a' || columna > 'h') {
+			
+			throw new IllegalArgumentException("Error: Columna no válida.");
+		}
+		this.columna = columna;
+	}
 	
 }
