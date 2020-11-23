36b7f09671c4a55a02d0a24d2ccd877865952fe3
Adrián Utrera Ruiz <adrianutreraruiz@gmail.com>
2020-11-23 13:05:22 +0100

    Creamos el constructor con los métodos get y set con las excepciones.

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
@@ -1,29 +1,30 @@
 package org.iesalandalus.programacion.alfilajedrez;
 
-
 public class Posicion {
 
 	private int fila;
 	private char columna;
-	
-	
+
 	public Posicion(int fila, char columna) {
-		setFila(fila);
-		setColumna(columna);		
+			setFila(fila);
+			setColumna(columna);
 	}
 	
 	public int getFila() {
 		return fila;
 	}
+	
 	private void setFila(int fila) {
 		if (fila <1 || fila >8) {
-			throw new IllegalArgumentException("Error: Fila no válida.");
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
 		}		
 		this.fila = fila;
 	}
-	public char getColunmna() {
+	
+	public char getColumna() {
 		return columna;
 	}
+	
 	private void setColumna(char columna) {
 		if (columna < 'a' || columna > 'h') {
 			
